b9cd1d8467412219887b4c242da1d94b2c436664
onderfrt <96356562+onderfrt@users.noreply.github.com>
2022-02-22 20:14:49 +0300

    Update Yönetimİslemler.cs
    bilanço eklendi.

--- IskurATM/IskurATM/Class/Yönetimİslemler.cs
+++ IskurATM/IskurATM/Class/Yönetimİslemler.cs
@@ -8,5 +8,20 @@ namespace IskurATM.Class
 {
     class Yönetimİslemler
     {
+        public static List<Yönetimİslemler> bilanco = new List<Yönetimİslemler>();
+        public int kisiId { get; set; }
+        public decimal Bakiye { get; set; }
+        public string Parabirimi { get; set; }
+        public DateTime Zaman { get; set; }
+        public bool BilancoEkle(int kisiID,decimal bakiye, string ParaBirimi)
+        {
+            Yönetimİslemler data = new Yönetimİslemler();
+            data.Bakiye = bakiye;
+            data.kisiId = kisiID;
+            data.Parabirimi = ParaBirimi;
+            bilanco.Add(data);
+            return true;
+        }
+
     }
 }
