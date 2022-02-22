d333cbb38d17f562903f019dbf9b117314aed3cb
Kerem Yılmaz <kerem.yilmaz9934@gmail.com>
2022-02-22 20:27:30 +0300

    Update Yönetim.cs
    Para transfer eklendi

--- IskurATM/IskurATM/Class/Yönetim.cs
+++ IskurATM/IskurATM/Class/Yönetim.cs
@@ -8,6 +8,26 @@ namespace IskurATM.Class
 {
     class Yönetim
     {
+        Yönetimİslemler yönetimİslemler = new Yönetimİslemler();
+        public void ParaTransfer(int gonderenID,int aliciID,decimal miktar)
+        {
+            Kisi gonderen = new Kisi();
+            Kisi alici = new Kisi();
+            gonderen = gonderen.KisiBul(gonderenID);
+            alici = alici.KisiBul(aliciID);
+            if (gonderen.bakiye-miktar<0)
+            {
+                Console.WriteLine("Yeterli bakiyeniz bulunmamaktadır");
+            }
+            else
+            {
+                gonderen.bakiye -= miktar;
+                alici.bakiye += miktar;
+                Console.WriteLine(gonderen.isim+" Kişisinden "+miktar+" miktar para "+alici.isim+" kişisine transfer edilmiştir.");
+                yönetimİslemler.BilancoEkle(gonderenID,gonderen.bakiye,"TL");
+            }
+        }
+
         /*
          para çekme,para yatırma ile bakiye hesaplanacak.
          para birimi önemli
